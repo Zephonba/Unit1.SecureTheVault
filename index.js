151cd0f0b95846c3a0f8f5d06b51aa3e571282aa
Zephon <Zephonbalthazar@yahoo.com>
2025-05-05 21:28:40 -0500

    vault secured

---
+++ index.js
@@ -0,0 +1,23 @@
+// Declare and assign code1
+let code1;
+code1 = 7 + 3; // (7 + 3) = 10
+
+// Declare and assign code2 using subtraction and division
+let code2 = (100 - 20) / 2; // (100 - 20) = 80; 80 / 2 = 40
+
+// Declare and assign code3
+let code3 = 80 % 41; // (80 % 41) = 39
+
+// Message
+let Message = "The vault has been secured. The combination is:";
+
+// Concatenate variables for codeA
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+// Template literal for codeB
+let codeB = `${code1}-${code2}-${code3}`;
+
+// Print to console
+console.log(Message);
+console.log(codeA);
+console.log(codeB);
